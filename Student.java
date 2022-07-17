ff0646115fea23a30ead9e000a1d2f4c7117f85d
UsmanNG <ugarba64@gmail.com>
2022-07-18 00:37:31 +0100

    Using indexes

--- Student.java
+++ Student.java
@@ -3,18 +3,19 @@ public class Student {
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'S';
-        char studentLastInitial = 'Y';
         boolean hasPerfectAttendance = true;
+
         String studentFirstName = "Shehu";
         String studentLastName = "Yakubu";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
         System.out.println(studentFirstName);
         System.out.println(studentLastName);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
     }
 }
