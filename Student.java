f9c344c64f3b889d0275533b8b621ea144bfba83
UsmanNG <ugarba64@gmail.com>
2022-07-18 00:27:49 +0100

    variables and data types

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Student {
+
+    public static void main(String[] args) {
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'S';
+        char studentLastInitial = 'Y';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+    }
+}
