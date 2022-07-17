494ce5c52df44f2cdcb7fdc666434d3e6b3900d4
UsmanNG <ugarba64@gmail.com>
2022-07-18 00:30:42 +0100

    Strings

--- Student.java
+++ Student.java
@@ -6,11 +6,15 @@ public class Student {
         char studentFirstInitial = 'S';
         char studentLastInitial = 'Y';
         boolean hasPerfectAttendance = true;
+        String studentFirstName = "Shehu";
+        String studentLastName = "Yakubu";
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
     }
 }
