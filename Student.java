e41b6bfc79d5d0e7cd9eedfc0dadde626b4f36fa
UsmanNG <ugarba64@gmail.com>
2022-07-18 00:43:12 +0100

    String concatenation

--- Student.java
+++ Student.java
@@ -1,21 +1,28 @@
 public class Student {
 
     public static void main(String[] args) {
-        int studentAge = 15;
-        double studentGPA = 3.45;
-        boolean hasPerfectAttendance = true;
+    //     int studentAge = 15;
+    //     double studentGPA = 3.45;
+    //     boolean hasPerfectAttendance = true;
 
+    //     String studentFirstName = "Shehu";
+    //     String studentLastName = "Yakubu";
+    //     char studentFirstInitial = studentFirstName.charAt(0);
+    //     char studentLastInitial = studentLastName.charAt(0);
+
+    //     System.out.println(studentAge);
+    //     System.out.println(studentGPA);
+    //     System.out.println(hasPerfectAttendance);
+    //     System.out.println(studentFirstName);
+    //     System.out.println(studentLastName);
+    //     System.out.println(studentFirstInitial);
+    //     System.out.println(studentLastInitial);
+
+        // String Concatenation
+        double studentGPA = 3.45;
         String studentFirstName = "Shehu";
         String studentLastName = "Yakubu";
-        char studentFirstInitial = studentFirstName.charAt(0);
-        char studentLastInitial = studentLastName.charAt(0);
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(hasPerfectAttendance);
-        System.out.println(studentFirstName);
-        System.out.println(studentLastName);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
     }
 }
