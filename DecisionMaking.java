7b16c32519cdf9fb5340cd1cd40a344f3888a831
UsmanNG <ugarba64@gmail.com>
2022-07-18 01:41:22 +0100

    looping

--- DecisionMaking.java
+++ DecisionMaking.java
@@ -3,16 +3,31 @@ import java.util.Scanner;
 public class DecisionMaking {
 
     public static void main(String[] args) {
-        System.out.println("Pick a number between 1 and 10");
-        Scanner scanner = new Scanner(System.in);
+        // System.out.println("Pick a number between 1 and 10");
+        // Scanner scanner = new Scanner(System.in);
 
-        int inputedNum = scanner.nextInt();
+        // int inputedNum = scanner.nextInt();
         
         // If Statement
-        if (inputedNum < 5) {
+        /*if (inputedNum < 5) {
             System.out.println("Enjoy the good luck a friend brings you");
         } else {
             System.out.println("Your selection will make you very happy today");
+        }*/
+
+        // While Statement
+        Scanner input = new Scanner(System.in);
+        boolean isOnRepeat = true;
+
+        while (isOnRepeat) {
+            System.out.println("Playing current song");
+            System.out.println("Would you like to take this song off of repeat? If so, answer yes");
+            String userInput = input.next();
+
+            if (userInput.equals("yes")) {
+                isOnRepeat = false;
+            }
         }
+        System.out.println("Playing next song");
     }
 }
